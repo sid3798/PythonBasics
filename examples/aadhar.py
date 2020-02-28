f7b8cba5edd522e365ca9c2da37c2c2ca28c153b
Neemit Shastri <neemitshastri05@gmail.com>
2020-02-28 17:33:42 +0530

    added aadhar for dict and list explanation

---
+++ examples/aadhar.py
@@ -0,0 +1,5 @@
+india_population = [['ramesh,29,delhi'],["suresh",42,"mumbai"],["mukesh",43,"Navi Mumbai"]]
+print(india_population)
+
+india_aadhar = {"AADHAR01":["ramesh",29,'delhi'],"AADHAR02":["suresh",32,"Mumbai"]}
+print(india_aadhar['AADHAR02'])
